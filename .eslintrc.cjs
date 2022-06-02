2a323f845d743331f3d6cce6cb105f52899c4808
Karma Blackshaw <irenesejah29@gmail.com>
2022-06-02 14:09:11 +0800

    update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,9 +3,18 @@ module.exports = {
   "root": true,
   "extends": [
     "plugin:vue/vue3-essential",
+    "plugin:vue/vue3-recommended",
+    "plugin:vue/vue3-strongly-recommended",
     "eslint:recommended"
   ],
   "env": {
-    "vue/setup-compiler-macros": true
-  }
+    "vue/setup-compiler-macros": true,
+    "browser": true,
+    "es2021": true,
+    "node": true
+  },
+  "parserOptions": {
+    "ecmaVersion": 12,
+    "sourceType": "module"
+  },
 }
