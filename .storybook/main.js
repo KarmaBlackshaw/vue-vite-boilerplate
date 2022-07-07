dffa620cdfb8a64d942d53542994c138c1186817
Karma Blackshaw <irenesejah29@gmail.com>
2022-07-07 13:43:46 +0800

    feat: apply unplugins in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,24 @@
+const path = require("path");
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+  viteFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "../src/"),
+    };
+
+    config.plugins.push(...[
+      require('../src/config/unplugin-auto-import'),
+      require('../src/config/unplugin-vue-components'),
+    ]);
+    // keep this if you're doing typescript
+    // config.resolve.extensions.push(".ts", ".tsx");
+    return config;
+  },
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
