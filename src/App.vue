89f83949e6d1584e8999b03adb8a3494cc70cddf
Karma Blackshaw <irenesejah29@gmail.com>
2022-07-07 13:44:39 +0800

    feat: add layouts folder

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,12 @@
+
+<script setup>
+import BaseLayout from '@/layouts/BaseLayout.vue'
+
+const layout = computed(() => {
+  return BaseLayout
+})
+</script>
+
 <template>
-  <main class="app">
-    <div class="app__navigation">
-      <router-link to="/">
-        Home
-      </router-link>
-
-      <router-link to="/about">
-        About
-      </router-link>
-    </div>
-
-    <div class="app__content">
-      <RouterView />
-    </div>
-  </main>
+  <layout />
 </template>
-
-<style lang="scss">
-body, html, #app, .app {
-  height: 100%;
-  font-family: "Quicksand", sans-serif;
-}
-
-.app {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  background-color: theme('colors.gray.900');
-  color: theme('colors.gray.100');
-
-  .app__navigation {
-    width: 100px;
-    text-align: center;
-    display: flex;
-    justify-content: center;
-    gap: 10px;
-    font-size: 13px;
-    margin-bottom: 10px;
-  }
-
-  .app__content {
-    height: 300px;
-    width: 400px;
-  }
-}
-</style>
---
+++ src/layouts/BaseLayout.vue
@@ -0,0 +1,48 @@
+<template>
+  <main class="app">
+    <div class="app__navigation">
+      <router-link to="/">
+        Home
+      </router-link>
+
+      <router-link to="/about">
+        About
+      </router-link>
+    </div>
+
+    <div class="app__content">
+      <RouterView />
+    </div>
+  </main>
+</template>
+
+<style lang="scss">
+body, html, #app, .app {
+  height: 100%;
+  font-family: "Quicksand", sans-serif;
+}
+
+.app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color: theme('colors.gray.900');
+  color: theme('colors.gray.100');
+
+  .app__navigation {
+    width: 100px;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    gap: 10px;
+    font-size: 13px;
+    margin-bottom: 10px;
+  }
+
+  .app__content {
+    height: 300px;
+    width: 400px;
+  }
+}
+</style>
