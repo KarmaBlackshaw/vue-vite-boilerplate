d84437589ad74401a0824332cef5f5f7b0b9b9fc
Karma Blackshaw <irenesejah29@gmail.com>
2022-07-07 17:04:39 +0800

    feat: base theme toggle component

---
+++ src/components/base-theme-toggle/BaseThemeToggle.vue
@@ -0,0 +1,42 @@
+<script setup>
+const {
+  isDark,
+  toggle: toggleColorScheme
+} = useColorScheme()
+
+</script>
+
+<template>
+  <div class="base-theme-toggle">
+    <input
+      id="checkbox"
+      type="checkbox"
+      class="checkbox"
+      :checked="isDark"
+      @change="toggleColorScheme()"
+    >
+
+    <label
+      for="checkbox"
+      class="label"
+    >
+
+      <div class="ball">
+        <img
+          v-if="isDark"
+          src="./assets/images/sun-3.png"
+          alt=""
+        >
+        <img
+          v-else
+          src="./assets/images/moon-3.png"
+          alt=""
+        >
+      </div>
+    </label>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import './BaseThemeToggle';
+</style>
