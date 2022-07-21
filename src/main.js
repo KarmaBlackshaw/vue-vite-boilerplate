2135d4dab588ef675546992e9e83170c9ce3aae7
karmablackshaw <irenesejah29@gmail.com>
2022-07-22 01:37:42 +0800

    refactor main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
+// vue core
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// scss
 import '@/assets/styles/app/index.scss'
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+createApp(App)
+  .use(router)
+  .mount('#app')
