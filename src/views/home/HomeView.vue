e2b5831eaebd5b5d021e7cb0b68c2c4f0ce51ec5
karmablackshaw <irenesejah29@gmail.com>
2022-07-22 01:37:22 +0800

    imrpove home layout

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,22 +1,59 @@
+<script setup>
+const {
+  t,
+  language
+} = useTranslate()
+
+</script>
+
 <template>
   <div>
-    <h1>Vue + Vite Boilerplate 🚀</h1>
+    <div class="text-center">
+      <span class="text-sm ">
+        {{ t('intro.greeting', { person: 'developer' }) }}
+      </span>
+    </div>
+
+    <h1>
+      Vue + Vite Boilerplate 🚀
+    </h1>
 
     <ul>
-      <li>
-        ⚡ Vue 3 and Vite 2
+      <li class="mb-3">
+        <EmojioneV1LightningMood class="inline mr-2" />
+
+        Vue 3 and Vite 2
       </li>
-      <li>
-        📦 Auto import components and apis on-demand
+      <li class="mb-3">
+        <VscodeIconsFileTypePackage class="inline mr-2" />
+        Auto import components and apis on-demand
       </li>
-      <li>
-        📖 Document components with storybook
+      <li class="mb-3">
+        <IconParkNewspaperFolding class="inline mr-2" />
+        Document components with storybook
       </li>
-      <li>
-        🎨 Tailwind
+      <li class="mb-3">
+        <VscodeIconsFileTypeTailwind class="inline mr-2" />
+        Tailwind
       </li>
-      <li>
-        🔃 NProgress
+      <li class="mb-3">
+        <MdiProgressHelper class="inline mr-2" /> NProgress
+      </li>
+      <li class="mb-3">
+        <VscodeIconsFileTypeLocale class="inline mr-2" />
+
+        <select
+          v-model="language"
+          class="w-[200px] px-2 py-1 rounded text-gray-900 "
+        >
+          <option value="en">
+            En
+          </option>
+
+          <option value="kr">
+            Kr
+          </option>
+        </select>
       </li>
     </ul>
   </div>
