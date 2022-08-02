2dfae79df1b0f073722df96d9881a9acc5393de4
Karma Blackshaw <irenesejah29@gmail.com>
2022-08-02 14:37:40 +0800

    add release it

---
+++ .release-it.js
@@ -0,0 +1,30 @@
+module.exports = {
+  hooks: {
+    'after:bump': ['npx auto-changelog -p'],
+    'after:git-release': [
+      'git add CHANGELOG.md',
+      'git push origin master',
+      'echo Successfully released ${name} v${version} to ${repo.repository}.'
+    ]
+  },
+  git: {
+    requireBranch: 'master',
+    commit: true,
+    commitMessage: 'chore(release): ${version}',
+    commitArgs: '',
+    tag: true,
+    tagName: '${version}',
+    tagAnnotation: '${version}',
+    push: true,
+    requireCommits: true,
+    changelog:
+      'npx auto-changelog --stdout --commit-limit false -u --template ./config/changelog.hbs'
+  },
+  github: {
+    release: false,
+    releaseName: '${version}'
+  },
+  npm: {
+    publish: false
+  }
+}
