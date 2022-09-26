c30beb1679d94f44a17767be1c9e53f2955611da
Karma Blackshaw <irenesejah29@gmail.com>
2022-09-26 17:34:21 +0800

    remove unused utils

--- src/utils/array.js
+++
@@ -1,7 +0,0 @@
-export function toArray (arr) {
-  return Array.isArray(arr) ? arr : [arr]
-}
-
-export function last (arr) {
-  return arr[arr.length - 1]
-}
--- src/utils/math.js
+++
@@ -1,3 +0,0 @@
-export function sum (arr) {
-  return arr.reduce((acc, curr) => acc + Number(curr))
-}
